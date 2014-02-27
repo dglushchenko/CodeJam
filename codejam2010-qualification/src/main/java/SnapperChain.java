88ff53cfee779b96a8fe6d6d5f672c351fe6711a
dglushchenko <denis.box@gmail.com>
2014-02-26 22:28:39 -0800

    complete solution

--- codejam2010-qualification/src/main/java/SnapperChain.java
+++ codejam2010-qualification/src/main/java/SnapperChain.java
@@ -1,17 +1,19 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.util.Scanner;
 
 public class SnapperChain {
 
-	/*private static long pow(long base, long exp) {
-		long result = 1;
-		
-		while (exp-- > 0) {
-			result *= base;
-		}
-		
-		return result;
-	}*/
-	
+	/*
+	 * private static long pow(long base, long exp) { long result = 1;
+	 * 
+	 * while (exp-- > 0) { result *= base; }
+	 * 
+	 * return result; }
+	 */
+
 	private static long pow(long base, long exp) {
 		long result = 1;
 		while (exp > 0) {
@@ -26,18 +28,28 @@ public class SnapperChain {
 	}
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int numberOfTestCases = scanner.nextInt();
-		for (int i = 1; i <= numberOfTestCases; i++) {
-			long n = scanner.nextLong();
-			long k = scanner.nextLong();
-			
-			System.out.print("Case #" + i + ": ");
-			long mask = pow(2, n) - 1;
-			if ((mask & k) == mask) {
-				System.out.println("ON");
-			} else {
-				System.out.println("OFF");
+		PrintWriter writer = null;
+		try {
+			Scanner scanner = new Scanner(new FileInputStream("A-large-practice.in"));
+			writer = new PrintWriter("A-large-practice.out");
+			int numberOfTestCases = scanner.nextInt();
+			for (int i = 1; i <= numberOfTestCases; i++) {
+				long n = scanner.nextLong();
+				long k = scanner.nextLong();
+
+				writer.print("Case #" + i + ": ");
+				long mask = pow(2, n) - 1;
+				if ((mask & k) == mask) {
+					writer.println("ON");
+				} else {
+					writer.println("OFF");
+				}
+			}
+		} catch (FileNotFoundException fnfe) {
+			fnfe.printStackTrace();
+		} finally {
+			if (writer != null) {
+				writer.close();
 			}
 		}
 	}
